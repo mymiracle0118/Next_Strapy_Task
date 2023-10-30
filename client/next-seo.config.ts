f9584a386a30b6cbd352cd19a2280f42b0503ed8
mymiracle0118 <noahsflood908@gmail.com>
2023-10-30 11:23:22 +0900

    fix viewport

--- client/next-seo.config.ts
+++ client/next-seo.config.ts
@@ -20,7 +20,7 @@ const config: DefaultSeoProps = {
   additionalMetaTags: [
     {
       name: "viewport",
-      content: "width=device-width, initial-scale=0.5",
+      content: "width=device-width, initial-scale=1",
     },
     {
       name: "keywords",
